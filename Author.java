b1aa5f2da45d13f4e1257e5e429f30a21c77eabf
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-27 00:29:30 +0530

    Add new code generated at 2024-06-27T00:29:27.978806

--- Author.java
+++ Author.java
@@ -1,8 +1,30 @@
-public class Author {
+public class BookController {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String description;
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Integer id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Integer id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Integer id) {
+        bookService.deleteBook(id);
+    }
 }
