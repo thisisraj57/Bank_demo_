4bc27671a00774bbb2a684c1b5eb1870d9076248
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-26 23:56:21 +0530

    Add new code generated at 2024-06-26T23:56:18.025879

--- AccountManagementApplication.java
+++ AccountManagementApplication.java
@@ -1,29 +1,5 @@
 public class AccountManagementApplication {
-
-    @Autowired
-    private AccountService accountService;
-
-    public static void main(String[] args) {
-        SpringApplication.run(AccountManagementApplication.class, args);
-    }
-
-    @PostMapping("/accounts")
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account);
-    }
-
-    @GetMapping("/accounts/{id}")
-    public Account getAccount(@PathVariable Long id) {
-        return accountService.getAccount(id);
-    }
-
-    @PutMapping("/accounts/{id}")
-    public Account updateAccount(@PathVariable Long id, @RequestBody Account account) {
-        return accountService.updateAccount(id, account);
-    }
-
-    @DeleteMapping("/accounts/{id}")
-    public void deleteAccount(@PathVariable Long id) {
-        accountService.deleteAccount(id);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(AccountManagementApplication.class, args);
+  }
 }
