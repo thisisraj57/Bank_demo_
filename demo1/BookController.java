a964bc66999d0da85b37fddde0b85ae4ccffda4f
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-27 13:35:49 +0530

    Add new code generated at 2024-06-27T13:35:46.253996

--- demo1/BookController.java
+++ demo1/BookController.java
@@ -1,30 +1,6 @@
-public class BookController {
+public class LibraryManagementApplication {
 
-    @Autowired
-    private BookService bookService;
-
-    @GetMapping
-    public List<Book> getAllBooks() {
-        return bookService.getAllBooks();
-    }
-
-    @GetMapping("/{id}")
-    public Book getBookById(@PathVariable Long id) {
-        return bookService.getBookById(id);
-    }
-
-    @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookService.createBook(book);
-    }
-
-    @PutMapping("/{id}")
-    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
-        return bookService.updateBook(id, book);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable Long id) {
-        bookService.deleteBook(id);
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementApplication.class, args);
     }
 }
