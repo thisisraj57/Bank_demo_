ff23fd2aced264bd6e23a8a342ff5f67700bf076
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-28 12:43:59 +0530

    Add new code generated at 2024-06-28T12:43:55.658574

---
+++ Bank_demo_/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String accountNumber;
+  private String customerName;
+  private String accountType;
+  // ... Other attributes and methods
+}
