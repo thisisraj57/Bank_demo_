7140cb121b35651552df27f8f7d56ceeb5f54e95
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-28 12:44:10 +0530

    Add new code generated at 2024-06-28T12:44:07.658829

---
+++ Bank_demo_/Author.java
@@ -0,0 +1,30 @@
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Integer id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Integer id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Integer id) {
+        bookService.deleteBook(id);
+    }
+}
