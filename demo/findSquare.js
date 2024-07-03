6737afbb25ac42a56ebdb7e07abced8613119935
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-07-03 18:07:17 +0530

    Create findSquare.js

---
+++ demo/findSquare.js
@@ -0,0 +1,11 @@
+// function to find square of a number
+function findSquare(num) {
+
+    // return square
+    return num * num; 
+}
+
+// call the function and store the result
+let square = findSquare(3);
+
+console.log(`Square: ${square}`);
