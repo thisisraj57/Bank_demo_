5c271a0439e7a44a6e71e593694e8c5b64cd155d
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-07-03 18:07:41 +0530

    Create squareRoot.js

---
+++ demo/squareRoot.js
@@ -0,0 +1,12 @@
+// Math.sqrt() computes the square root
+let squareRoot = Math.sqrt(4);
+console.log("Square Root of 4 is", squareRoot);
+
+// Math.pow() computes the power
+let power = Math.pow(2, 3);
+console.log("2 to the power of 3 is", power);
+
+// toUpperCase() converts text to uppercase
+let band = "Iron Maiden";
+let bandUpper = band.toUpperCase();
+console.log(`Favorite Band: ${bandUpper}`);
