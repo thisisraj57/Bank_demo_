66d22592f3d0684dc8966fa10f3f946c5f68fbc0
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-07-03 18:08:17 +0530

    Create Promise.js

---
+++ demo/Promise.js
@@ -0,0 +1,12 @@
+// returns a promise
+let countValue = new Promise(function (resolve, reject) {
+    // could be resolved or rejected   
+    resolve('Promise resolved'); 
+});
+
+// add other blocks of code
+countValue.finally(
+    function greet() {
+        console.log('This code is executed.');
+    }
+);
