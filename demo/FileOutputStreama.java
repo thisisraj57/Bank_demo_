2cb284fdf4d447ed6ee01ab4130635ea90ac9c51
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-07-03 17:57:06 +0530

    Create FileOutputStreama.java

---
+++ demo/FileOutputStreama.java
@@ -0,0 +1,27 @@
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String data = "This is a line of text inside the file";
+
+        try {
+            // Creates a FileOutputStream
+            FileOutputStream file = new FileOutputStream("output.txt");
+
+            // Creates a BufferedOutputStream
+            BufferedOutputStream output = new BufferedOutputStream(file);
+
+            byte[] array = data.getBytes();
+
+            // Writes data to the output stream
+            output.write(array);
+            output.close();
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
