8bd682bc4675b9c31dd00b9aa29f55e0bdb05fa8
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-07-03 18:08:54 +0530

    Create constructor.js

---
+++ demo/constructor.js
@@ -0,0 +1,16 @@
+// declare a class
+class Person {
+
+    // constructor function
+    constructor(name) {
+        this.name = name;
+    };
+};
+
+// create objects
+let p1 = new Person("John");
+let p2 = new Person("Rachel");
+
+// print object properties
+console.log(p1.name);  // John
+console.log(p2.name);  // Rachel
